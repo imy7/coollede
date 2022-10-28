1aba93b6b08560998031566409a2d8694a2ac9aa
imy7 <34760601+imy7@users.noreply.github.com>
2022-10-28 08:29:05 +0800

    Add files via upload

---
+++ R4S/part1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
+svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
+sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
+sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+#echo 'src-git ikoolproxy https://github.com/yaof2/luci-app-ikoolproxy' >>feeds.conf.default
+git clone https://github.com/kiddin9/openwrt-packages package/kiddn9
---
+++ amd64/part1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
+svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
+sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
+sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+git clone https://github.com/kiddin9/openwrt-packages package/kiddn9
