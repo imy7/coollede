8109efd95d666d06c4579bfc91847ab4f1d16691
imy7 <34760601+imy7@users.noreply.github.com>
2022-11-03 10:51:29 +0800

    Update part3.sh

--- amd64/part3.sh
+++ amd64/part3.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd openwrt/package
-git clone https://github.com/yaof2/luci-app-ikoolproxy 
 git clone https://github.com/sbwml/luci-app-mosdns
 git clone https://github.com/kiddin9/openwrt-bypass
 git clone https://github.com/siwind/luci-app-wolplus
 git clone https://github.com/imy7/luci-theme
+git clone https://github.com/kenzok8/small
+git clone https://github.com/fw876/helloworld
+git clone https://github.com/vernesong/OpenClash
+git clone https://github.com/xiaorouji/openwrt-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall
+
